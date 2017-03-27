d97547c21a53a1342e4832b2ce9a23bff2425522
tomi <tomi@ccfocusone.com>
2017-03-27 16:01:42 +0800

    [feature] add shorten filter

--- src/filters.js
+++ src/filters.js
@@ -7,7 +7,7 @@ export default {
   shortenNumber(num) {
     if (typeof num !== 'number') return 0;
 
-    return (num > 1000)
+    return (num >= 1000)
       ? `${parseFloat(num / 1000).toFixed(0)}K`
       : num;
   },
