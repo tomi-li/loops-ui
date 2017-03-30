768c224c6e5549945ba830c226d03cee7466c934
tomi <tomi@ccfocusone.com>
2017-03-30 14:56:36 +0800

    [feature] loading backdrop

--- src/Page.vue
+++ src/Page.vue
@@ -41,7 +41,7 @@
       left: 0;
       height: 100%;
       width: 100%;
-      background-color: rgba(171, 173, 187, .8);
+      background-color: rgba(171, 173, 187, .5);
       z-index: 100;
     }
 
