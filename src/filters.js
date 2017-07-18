20f981610d87919354300ef3040fc07aaa6ab39d
Tomi <lishuo.tomi@gmail.com>
2017-07-18 17:34:04 +0800

    [fix] circle shorten number

--- src/filters.js
+++ src/filters.js
@@ -9,9 +9,12 @@ export default {
 
     if (num >= 1000) {
       if (num % 1000 === 0) {
-        return `${parseFloat(num / 1000).toFixed(0)}K`;
+        return `${parseFloat(num / 1000).toPrecision(0)}K`;
       }
-      return `${parseFloat(num / 1000).toFixed(1)}K`;
+      // in Chrome. to Precision will auto rounding. using string regex instead
+      // return `${parseFloat(num / 1000).toPrecision(1)}K`;
+      const with1Decimals = (num / 1000).toString().match(/^-?\d+(?:\.\d)?/)[0];
+      return parseFloat(with1Decimals);
     }
 
     return num;
